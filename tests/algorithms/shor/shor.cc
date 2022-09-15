bbbed24a568d101784c396369a7711a444db42bc
pablolh <p.lehenaff@tudelft.nl>
2022-09-15 15:22:26 +0200

    Add .clang-format and format all .cc files

--- tests/algorithms/shor/shor.cc
+++ tests/algorithms/shor/shor.cc
@@ -1,18 +1,9 @@
-
 #include <xpu.h>
 #include <xpu/runtime>
 
-
 #include <core/circuit.h>
-
-
 
 /**
  * main
  */
-int main(int argc, char ** argv)
-{
-
-
-   return 0;
-}
+int main(int argc, char **argv) { return 0; }
--- tests/qxelarator/test_multiple_execution.cc
+++ tests/qxelarator/test_multiple_execution.cc
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "qx/simulator.h"
+#include <iostream>
 
 int main() {
 
