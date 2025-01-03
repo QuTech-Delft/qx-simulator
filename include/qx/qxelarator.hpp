00eb76b148445b00cb70670f090d72d27c1a26d4
rturrado <rturrado@gmail.com>
2025-01-04 00:24:54 +0100

    Add doxygen doc strings in include/qx/qxelarator.hpp and python/qxelarator/__init__.py. Rename variable 's' to 'program'.

--- include/qx/qxelarator.hpp
+++ include/qx/qxelarator.hpp
@@ -6,13 +6,36 @@
 
 namespace qxelarator {
 
-inline std::variant<std::monostate, qx::SimulationResult, qx::SimulationError> execute_string(std::string const& s,
-    std::size_t iterations = 1, std::optional<std::uint_fast64_t> seed = std::nullopt, std::string version = "3.0") {
-    return qx::execute_string(s, iterations, seed, std::move(version));
+/**
+ * Simulates a cQASM program from a string.
+ *
+ * Parameters:
+ *   - `program` is a string containing the cQASM program.
+ *   - `iterations` is the number of times the program is being simulated.
+ *   - `seed` can be used in case a deterministic output is needed.
+ *   - `version` is the cQASM program version.
+ *
+ * Returns either a simulation result or an error.
+ */
+inline std::variant<std::monostate, qx::SimulationResult, qx::SimulationError> execute_string(
+    const std::string& program, std::size_t iterations = 1, std::optional<std::uint_fast64_t> seed = std::nullopt,
+    std::string version = "3.0") {
+    return qx::execute_string(program, iterations, seed, std::move(version));
 }
 
+/**
+ * Simulates a cQASM program from a file.
+ *
+ * Parameters:
+ *   - `file_path` is the file path of the cQASM program.
+ *   - `iterations` is the number of times the program is being simulated.
+ *   - `seed` can be used in case a deterministic output is needed.
+ *   - `version` is the cQASM program version.
+ *
+ * Returns either a simulation result or an error.
+ */
 inline std::variant<std::monostate, qx::SimulationResult, qx::SimulationError> execute_file(
-    std::string const& file_path, std::size_t iterations = 1, std::optional<std::uint_fast64_t> seed = std::nullopt,
+    const std::string& file_path, std::size_t iterations = 1, std::optional<std::uint_fast64_t> seed = std::nullopt,
     std::string version = "3.0") {
     return qx::execute_file(file_path, iterations, seed, std::move(version));
 }
