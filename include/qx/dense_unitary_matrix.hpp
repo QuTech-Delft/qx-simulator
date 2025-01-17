fb807445ea89926ddf9e6120eafc529e7357fa98
rturrado <rturrado@gmail.com>
2025-01-17 22:58:00 +0100

    Implement and test cofactor, determinant, adjoint... Add tests.

--- include/qx/dense_unitary_matrix.hpp
+++ include/qx/dense_unitary_matrix.hpp
@@ -13,11 +13,62 @@
 namespace qx::core {
 
 template <std::size_t N>
+using Matrix = std::array<std::array<std::complex<double>, N>, N>;
+
+template <size_t N>
+constexpr Matrix<N-1> cofactor(const Matrix<N>& m, size_t src_row_index_to_delete, size_t src_col_index_to_delete)
+requires (N > 1) {
+    Matrix<N-1> ret{};
+    size_t dst_row_index{ 0 };
+    for (std::size_t src_row_index = 0; src_row_index < N; ++src_row_index) {
+        if (src_row_index != src_row_index_to_delete) {
+            size_t dst_col_index{ 0 };
+            for (std::size_t src_col_index = 0; src_col_index < N; ++src_col_index) {
+                if (src_col_index != src_col_index_to_delete) {
+                    ret[dst_row_index][dst_col_index] = m[src_row_index][src_col_index];
+                    dst_col_index++;
+                }
+            }
+            dst_row_index++;
+        }
+    }
+    return ret;
+}
+
+template <size_t N = 1>
+constexpr std::complex<double> determinant(const Matrix<N>& m){
+    return m[0][0];
+}
+
+// This code is supposed to be used with unitary matrices, which cannot be singular
+// Thus, no check for singular matrix is done
+template <size_t N>
+constexpr std::complex<double> determinant(const Matrix<N>& m) requires (N > 1) {
+    std::complex<double> ret{};
+    std::complex<double> sign{ 1 };
+    for (size_t j = 0; j < N; ++j) {
+        ret += sign * m[0][j] * determinant(cofactor(m, 0, j));
+        sign *= -1;
+    }
+    return ret;
+}
+
+template <size_t N>
+constexpr Matrix<N> adjoint(const Matrix<N>& m) {
+    Matrix<N> ret;
+    for (std::size_t i = 0; i < N; ++i) {
+        for (std::size_t j = 0; j < N; ++j) {
+            auto sign = ((i + j) % 2 == 0) ? 1 : -1;
+            ret[j][i] = static_cast<std::complex<double>>(sign) * determinant(cofactor(m, i, j));
+        }
+    }
+    return ret;
+}
+
+template <std::size_t N>
 class DenseUnitaryMatrix {
 public:
-    using Matrix = std::array<std::array<std::complex<double>, N>, N>;
-
-    explicit constexpr DenseUnitaryMatrix(const Matrix& m)
+    explicit constexpr DenseUnitaryMatrix(const Matrix<N>& m)
     : DenseUnitaryMatrix{ m, true } {}
 
     [[nodiscard]] constexpr std::complex<double>& at(std::size_t i, std::size_t j) {
@@ -40,7 +91,7 @@ public:
     }
 
     constexpr DenseUnitaryMatrix<N> operator*(const DenseUnitaryMatrix<N>& other) const {
-        Matrix m{};
+        Matrix<N> m{};
         for (std::size_t i = 0; i < N; ++i) {
             for (std::size_t j = 0; j < N; ++j) {
                 m[i][j] = 0;
@@ -52,18 +103,20 @@ public:
         return DenseUnitaryMatrix{ m, false };
     }
 
-    static constexpr DenseUnitaryMatrix<N> identity() {
-        Matrix m{};
-        for (std::size_t i = 0; i < N; ++i) {
-            for (std::size_t j = 0; j < N; ++j) {
-                m[i][j] = (i == j) ? 1 : 0;
+    constexpr DenseUnitaryMatrix<N*2> control() const {
+        auto ret{ DenseUnitaryMatrix<N*2>::identity() };
+        std::size_t first_index = N;
+        std::size_t last_index = N*2;
+        for (std::size_t i = first_index; i < last_index; ++i) {
+            for (std::size_t j = first_index; j < last_index; ++j) {
+                ret.at(i, j) = matrix[i - first_index][j - first_index];
             }
         }
-        return DenseUnitaryMatrix{ m, false };
+        return ret;
     }
 
     constexpr DenseUnitaryMatrix<N> dagger() const {
-        Matrix m{};
+        Matrix<N> m{};
         for (std::size_t i = 0; i < N; ++i) {
             for (std::size_t j = 0; j < N; ++j) {
                 m[i][j] = std::conj(matrix[j][i]);
@@ -72,9 +125,25 @@ public:
         return DenseUnitaryMatrix{ m, false };
     }
 
+    static constexpr DenseUnitaryMatrix<N> identity() {
+        Matrix<N> m{};
+        for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t j = 0; j < N; ++j) {
+                m[i][j] = (i == j) ? 1 : 0;
+            }
+        }
+        return DenseUnitaryMatrix{ m, false };
+    }
+
     constexpr DenseUnitaryMatrix<N> inverse() const {
-        Matrix m{};
-        // TODO: implement
+        Matrix<N> m{};
+        auto det = determinant(*this);
+        auto adj = adjoint(m);
+        for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t j = 0; j < N; ++j) {
+                m[i][j] = adj[i][j] / det;
+            }
+        }
         return DenseUnitaryMatrix{ m, false };
     }
 
@@ -93,20 +162,8 @@ public:
         return DenseUnitaryMatrix{ m, false };
     }
 
-    constexpr DenseUnitaryMatrix<N*2> control() {
-        auto ret{ DenseUnitaryMatrix<N*2>::identity() };
-        std::size_t first_index = N;
-        std::size_t last_index = N*2 -1;
-        for (std::size_t i = first_index; i < last_index; ++i) {
-            for (std::size_t j = first_index; j < last_index; ++j) {
-                ret.at(i, j) = matrix[i - first_index][j - first_index];
-            }
-        }
-        return ret;
-    }
-
 private:
-    constexpr DenseUnitaryMatrix(const Matrix& m, bool is_unitary_check)
+    constexpr DenseUnitaryMatrix(const Matrix<N>& m, bool is_unitary_check)
     : matrix{ m } {
         if (is_unitary_check) {
             check_is_unitary();
@@ -119,7 +176,7 @@ private:
         }
     }
 
-    Matrix matrix;
+    Matrix<N> matrix;
 };
 
 template <std::size_t N>
--- test/dense_unitary_matrix_test.cpp
+++ test/dense_unitary_matrix_test.cpp
@@ -1,4 +1,5 @@
 #include "qx/dense_unitary_matrix.hpp"
+#include "qx/gates.hpp"
 
 #include <gmock/gmock.h>  // ThrowsMessage
 #include <gtest/gtest.h>
@@ -12,57 +13,109 @@ using namespace std::complex_literals;
 
 // clang-format off
 
+TEST(dense_unitary_matrix_test, reject_non_unitary_matrices) {
+    Matrix<2> non_unitary_1{{
+        { 1,   0 },
+        { 0, 1.1 }
+    }};
+    EXPECT_THAT([&non_unitary_1]() { DenseUnitaryMatrix<2> test{ non_unitary_1 }; },
+        ::testing::ThrowsMessage<std::runtime_error>("matrix is not unitary"));
+
+    Matrix<2> non_unitary_2{{
+        { 0, 0 },
+        { 0, 1 }
+    }};
+    EXPECT_THAT([&non_unitary_2]() { DenseUnitaryMatrix<2> test{ non_unitary_2 }; },
+        ::testing::ThrowsMessage<std::runtime_error>("matrix is not unitary"));
+
+    Matrix<3> non_unitary_3{{
+        { 1i,  0,  0 },
+        {  0, 1i, 1i },
+        {  0,  1,  0 }
+    }};
+    EXPECT_THAT([&non_unitary_3]() { DenseUnitaryMatrix<3> test{ non_unitary_3 }; },
+        ::testing::ThrowsMessage<std::runtime_error>("matrix is not unitary"));
+}
+
 TEST(dense_unitary_matrix_test, identity) {
-    DenseUnitaryMatrix<5> expected_identity({ {
+    DenseUnitaryMatrix<5> expected_identity{{{
         { 1, 0, 0, 0, 0 },
         { 0, 1, 0, 0, 0 },
         { 0, 0, 1, 0, 0 },
         { 0, 0, 0, 1, 0 },
         { 0, 0, 0, 0, 1 }
-    } });
-
+    }}};
     EXPECT_EQ(DenseUnitaryMatrix<5>::identity(), expected_identity);
 }
 
-TEST(dense_unitary_matrix_test, reject_non_unitary_matrices) {
-    DenseUnitaryMatrix<2>::Matrix non_unitary_1({ {
-        { 1,   0 },
-        { 0, 1.1 }
-    } });
-
-    EXPECT_THAT([&non_unitary_1]() { DenseUnitaryMatrix<2> test(non_unitary_1); },
-        ::testing::ThrowsMessage<std::runtime_error>("matrix is not unitary"));
-
-    DenseUnitaryMatrix<2>::Matrix non_unitary_2({ {
-        { 0, 0 },
-        { 0, 1 }
-    } });
-
-    EXPECT_THAT([&non_unitary_2]() { DenseUnitaryMatrix<2> test(non_unitary_2); },
-        ::testing::ThrowsMessage<std::runtime_error>("matrix is not unitary"));
-
-    DenseUnitaryMatrix<3>::Matrix non_unitary_3({ {
-        { 1i,  0,  0 },
-        {  0, 1i, 1i },
-        {  0,  1,  0 }
-    } });
-
-    EXPECT_THAT([&non_unitary_3]() { DenseUnitaryMatrix<3> test(non_unitary_3); },
-        ::testing::ThrowsMessage<std::runtime_error>("matrix is not unitary"));
-}
-
 TEST(dense_unitary_matrix_test, dagger) {
-    DenseUnitaryMatrix<2> m({ {
+    DenseUnitaryMatrix<2> m{{{
         {  1 / std::numbers::sqrt2,   1 / std::numbers::sqrt2 },
         { 1i / std::numbers::sqrt2, -1i / std::numbers::sqrt2 }
-    } });
-
-    DenseUnitaryMatrix<2> m_dag({ {
+    }}};
+    DenseUnitaryMatrix<2> m_dag{{{
         { 1 / std::numbers::sqrt2, -1i / std::numbers::sqrt2 },
         { 1 / std::numbers::sqrt2,  1i / std::numbers::sqrt2 }
-    } });
-
+    }}};
     EXPECT_EQ(m.dagger(), m_dag);
+}
+
+TEST(dense_unitary_matrix_test, cofactor) {
+    Matrix<4> m{{
+        {  5, -2,  2, 7 },
+        {  1,  0,  0, 3 },
+        { -3,  1,  5, 0 },
+        {  3, -1, -9, 4 },
+    }};
+    Matrix<3> expected{{
+        {  5,  2, 7 },
+        { -3,  5, 0 },
+        {  3, -9, 4 },
+    }};
+    EXPECT_EQ(cofactor(m, 1, 1), expected);
+}
+
+TEST(dense_unitary_matrix_test, determinant) {
+    Matrix<1> m1{{
+        { 5 }
+    }};
+    EXPECT_EQ(determinant(m1), std::complex<double>(5));
+
+    Matrix<3> m3{{
+        {  1,  0, 3 },
+        { -3,  5, 0 },
+        {  3, -9, 4 },
+    }};
+    EXPECT_EQ(determinant(m3), std::complex<double>(56));
+}
+
+TEST(dense_unitary_matrix_test, adjoint) {
+    Matrix<4> m{{
+        {  5, -2,  2, 7 },
+        {  1,  0,  0, 3 },
+        { -3,  1,  5, 0 },
+        {  3, -1, -9, 4 },
+    }};
+    Matrix<4> expected{{
+        { -12,  76, -60, -36 },
+        { -56, 208, -82, -58 },
+        {   4,   4,  -2, -10 },
+        {   4,   4,  20,  12 },
+    }};
+    EXPECT_EQ(adjoint(m), expected);
+}
+
+TEST(dense_unitary_matrix_test, inverse) {
+}
+
+TEST(dense_unitary_matrix_test, power) {
+
+}
+
+TEST(dense_unitary_matrix_test, control) {
+    EXPECT_EQ(gates::X.control(), gates::CNOT);
+    EXPECT_EQ(gates::Z.control(), gates::CZ);
+    EXPECT_EQ(gates::X.control().control(), gates::TOFFOLI);
 }
 
 // clang-format on
