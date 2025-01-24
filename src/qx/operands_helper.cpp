c80fa410a096e35405e368cd18f6efd50d07996d
rturrado <rturrado@gmail.com>
2025-01-24 22:56:49 +0100

    Fix bug in get_cqasm_v3x_sgmq_group_indices. Update documentation for to_cqasm_v3x_instructions_indices.

--- src/qx/operands_helper.cpp
+++ src/qx/operands_helper.cpp
@@ -42,16 +42,22 @@ namespace qx {
         }
         return ret;
     } else if (auto index_ref = operand.as_index_ref()) {
-        auto ret = index_ref->indices;
+        const auto& indices = index_ref->indices;
+        auto ret = CqasmV3xSgmqGroupIndices{};
+        ret.get_vec().resize(indices.size());
         if (is_qubit_variable(*index_ref->variable)) {
             auto qubit_range = register_manager.get_qubit_range(index_ref->variable->name);
-            std::for_each(ret.get_vec().begin(), ret.get_vec().end(), [qubit_range](const auto& index) {
-                index->value += qubit_range.first;
+            std::transform(indices.get_vec().begin(), indices.get_vec().end(), ret.get_vec().begin(),
+                [qubit_range](const auto& index) {
+                    return cqasm_tree::make<CqasmV3xConstInt>(
+                        static_cast<cqasm_v3x_primitives::Int>(qubit_range.first + index->value));
             });
         } else if (is_bit_variable(*index_ref->variable)) {
             auto bit_range = register_manager.get_bit_range(index_ref->variable->name);
-            std::for_each(ret.get_vec().begin(), ret.get_vec().end(), [bit_range](const auto& index) {
-                index->value += bit_range.first;
+            std::transform(indices.get_vec().begin(), indices.get_vec().end(), ret.get_vec().begin(),
+                [bit_range](const auto& index) {
+                    return cqasm_tree::make<CqasmV3xConstInt>(
+                        static_cast<cqasm_v3x_primitives::Int>(bit_range.first + index->value));
             });
         }
         return ret;
@@ -60,15 +66,17 @@ namespace qx {
     }
 }
 
-// Due to SGMQ, each operand of a cQASM v3x instruction can refer to multiple indices
-// So a list of cQASM v3x operands is called here SGMQ group
-// Now, after unrolling a SGMQ instruction, each unrolled instruction has a list of indices
-// This function parses the operands of an instruction and reorders them into a list of instructions indices
+// Due to Single Gate Multiple Qubit (SGMQ), each operand of a cQASM v3x instruction can contain a list of indices
+// Here, the term SGMQ group is used to refer to the list of indices of a cQASM v3x operand
+// Now, after unrolling an SGMQ instruction, each operand of the unrolled instruction will contain a single index
+// But the unrolled instruction, since it can have multiple operands, will contain a list of indices, one per operand
+// This function converts a list of operands with a list of indices (SGMQ groups)
+// into a list of instructions with a list of indices (instructions indices)
 //
 // For example, for CNOT q[0:2], q[3:5]
-// The operands (or SGMQ groups) are : [[0, 1, 2], [3, 4, 5]]
+// The list of operands with a list of indices (SGMQ groups) are: [[0, 1, 2], [3, 4, 5]]
 // After unrolling to: CNOT q[0], q[3]; CNOT q[1], q[4]; CNOT q[2], q[5]
-// The list of instructions indices will be: [[0, 3], [1, 4], [2, 5]]
+// The list of instructions with a list of indices (instructions indices) will be: [[0, 3], [1, 4], [2, 5]]
 [[nodiscard]] CqasmV3xInstructionsIndices to_cqasm_v3x_instructions_indices(
     const CqasmV3xSgmqGroupsIndices& sgmq_groups_indices) {
     if (sgmq_groups_indices.empty()) {
