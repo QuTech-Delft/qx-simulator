4472309c2d1c49120fd0941d83cbace1d384d667
Roberto Turrado Camblor <rturrado@gmail.com>
2025-03-24 13:18:19 +0100

    Update `master` to release 0.8.0 (#202)
    * Add doc workflow.
    
    * Update CHANGELOG.md.
    
    * Add requirements.txt.
    
    * Update SimulationIterationAccumulator:
    - get_simulation_result now receives shots_requested.
    - Add shots_done.
    - Remove measurements_count and bit_measurements_count.
    
    Update SimulationResult Python documentation:
    - shots_requested is the number of requested iterations.
    - shots_done is the number of performed iterations.
    
    * Update CHANGELOG.md.
    
    * Remove support for 'reset all'.
    
    * Update CHANGELOG.md.
    
    * Update src/qx/simulation_result.cpp.
    
    Co-authored-by: Chris Elenbaas <67630508+elenbaasc@users.noreply.github.com>
    
    * Add comment to get_simulation_result.
    
    * Fix get_simulation_result comment.
    
    * [CQT-321] Optimizations to `DenseUnitaryMatrix` and default gates (#189)
    
    Allow move constructing a DenseUnitaryMatrix.
    Change DenseUnitaryMatrix constructor to receive a unitary check flag.
    Add default constructors, destructor, and assignment operators for DenseUnitaryMatrix.
    Leave all default gate variable definitions as 'static', and all default gate function definitions as 'inline'.
    Do not check if a matrix is unitary for default gates.
    Update CHANGELOG.md.
    
    * [CQT-344] Fix issue #135 (#192)
    
    Enable address sanitizer for debug builds, except for clang/Linux.
    
    * Disable Windows/Debug job. (#196)
    
    * Update `requirements.txt` (#195)
    
    * Trying to fix 'Publish GitHub pages' job.
    
    * Update CHANGELOG.md.
    
    * [CQT-320] Integrate libQASM 1.0.0 into QX-simulator (#194)
    
    * Update libqasm version to 1.0.0.
    
    * Update CHANGELOG.md.
    
    * Trying to fix 'Publish GitHub pages' job.
    
    * Try to use latest Ubuntu versions for GitHub Actions jobs (#187)
    
    * Try to use ubuntu-latest for cpp-linux-x64 job.
    Try to use ubuntu-24.04-arm for cpp-linux-arm64 job.
    
    * Trying to fix clang/Linux jobs.
    
    * Update CHANGELOG.md.
    
    * Creating release 0.7.5. (#197)
    
    * [CQT-353] Integrate libQASM 1.1.0 into QX simulator (#199)
    
    * Update CHANGELOG.md.
    
    * Update conanfile.py to use libqasm/1.1.0.
    
    * Update cqasm::v3x::ast to cqasm::v3x::syntactic.
    
    * Fix build.
    
    * [CQT-316] Add support for `power` gate modifier with a fractional exponent (#188)
    
    * Update CHANGELOG.md.
    
    * Add Eigen dependency.
    
    * Implement power of matrices with a fractional exponent using Eigen.
    
    * Change PI and SQRT_2 types to double, as we don't use long double anywhere in the code.
    
    * Add 'pow' gate modifier with fractional exponent integration tests.
    
    * Add gates::SQRT_3.
    
    * Update ctest command to run tests in parallel.
    
    * Creating release 0.8.0. (#200)
    
    ---------
    
    Co-authored-by: Chris Elenbaas <67630508+elenbaasc@users.noreply.github.com>

--- test/dense_unitary_matrix.cpp
+++ test/dense_unitary_matrix.cpp
@@ -3,7 +3,7 @@
 #include <gmock/gmock.h>  // ThrowsMessage
 #include <gtest/gtest.h>
 
-#include <numbers>
+#include <complex>  // exp
 #include <stdexcept>  // runtime_error
 
 #include "qx/gates.hpp"
@@ -11,6 +11,9 @@
 namespace qx::core {
 
 using namespace std::complex_literals;
+
+using gates::PI;
+using gates::SQRT_2;
 
 // clang-format off
 
@@ -76,24 +79,65 @@ TEST(dense_unitary_matrix_test, dagger) {
 }
 
 TEST(dense_unitary_matrix_test, inverse) {
-    EXPECT_EQ(gates::RX(gates::PI/4).inverse(), gates::RX(-gates::PI/4));
+    EXPECT_EQ(gates::RX(PI/4).inverse(), gates::RX(-PI/4));
     EXPECT_EQ(gates::S.inverse(), gates::SDAG);
     EXPECT_EQ(gates::T.inverse(), gates::TDAG);
-    EXPECT_EQ(gates::X90.inverse(), gates::RX(-gates::PI/2));
+    EXPECT_EQ(gates::X90.inverse(), gates::RX(-PI/2));
 }
 
-TEST(dense_unitary_matrix_test, power) {
-    EXPECT_THAT([]() { (void) gates::H.power(1.5); },
-        ::testing::ThrowsMessage<std::runtime_error>("unimplemented: matrix power with a non-integer exponent"));
+TEST(dense_unitary_matrix_test, power_integer) {
     EXPECT_EQ(gates::H.power(2), gates::IDENTITY);
     EXPECT_EQ(gates::X.power(2), gates::IDENTITY);
     DenseUnitaryMatrix minus_identity{ Matrix{
         { -1,  0 },
         {  0, -1 }
     }};
-    EXPECT_EQ(gates::RX(gates::PI).power(2), minus_identity);
+    EXPECT_EQ(gates::RX(PI).power(2), minus_identity);
     EXPECT_EQ(gates::S.power(2), gates::Z);
     EXPECT_EQ(gates::T.power(4), gates::Z);
+}
+
+// The matrices defined here as powers of a default gate are valid roots computed by Eigen
+// For example, for sqrt(X), we get a unitary matrix equal to X90
+TEST(dense_unitary_matrix_test, power_fractional) {
+    const auto& x_power_1_2 = DenseUnitaryMatrix{
+        Matrix{
+            {  std::exp(std::complex<double>(0, PI/4.)) / SQRT_2, std::exp(std::complex<double>(0, -PI/4.)) / SQRT_2 },
+            { std::exp(std::complex<double>(0, -PI/4.)) / SQRT_2,  std::exp(std::complex<double>(0, PI/4.)) / SQRT_2 }
+        }
+    };
+    EXPECT_EQ(gates::X.power(1./2), x_power_1_2);  // X90
+
+    const auto& x_power_m1_2 = DenseUnitaryMatrix{
+        Matrix{
+            { std::exp(std::complex<double>(0, -PI/4.)) / SQRT_2,  std::exp(std::complex<double>(0, PI/4.)) / SQRT_2 },
+            {  std::exp(std::complex<double>(0, PI/4.)) / SQRT_2, std::exp(std::complex<double>(0, -PI/4.)) / SQRT_2 }
+        }
+    };
+    EXPECT_EQ(gates::X.power(-1./2), x_power_m1_2);  // MX90
+
+    EXPECT_EQ(gates::Z.power(1./2), gates::S);
+    EXPECT_EQ(gates::Z.power(-1./2), gates::SDAG);
+    EXPECT_EQ(gates::Z.power(1./4), gates::T);
+    EXPECT_EQ(gates::Z.power(-1./4), gates::TDAG);
+
+    const auto& s_power_1_2 = DenseUnitaryMatrix{
+        Matrix{
+            { 1,                                         0 },
+            { 0, std::exp(std::complex<double>(0, PI / 4)) }
+        }
+    };
+    EXPECT_EQ(gates::S.power(1./2), s_power_1_2);
+
+    const auto& z_power_1_3 = DenseUnitaryMatrix{
+        Matrix{
+            { 1,                                         0 },
+            { 0, std::exp(std::complex<double>(0, PI / 3)) }
+        }
+    };
+    EXPECT_EQ(gates::Z.power(1./3), z_power_1_3);
+
+    EXPECT_EQ(gates::Z.power(1./4).power(4).power(1./3).power(3).power(1./2).power(2), gates::Z);
 }
 
 TEST(dense_unitary_matrix_test, control) {
